ea072807268482012a15307f51f0b7d7ea19db1a
oreoluwa <gifttee2004@gmail.com>
2022-07-02 18:50:09 +0100

    modified to do list

--- script.js
+++ script.js
@@ -1,121 +1,99 @@
 
 let inputElement = document.querySelector('#todo-myinput');
 let formElement  = document.querySelector('#form');
-let mylist  =  document.querySelector('ul');
+let post=  document.querySelector('#post');
 let clearitem = document.querySelector('#clear');
 
-let tasklist = [
-    "buy gorceries",
-    "buy books",
-]
+formElement.addEventListener("submit", (e) =>{
+ e.preventDefault();
+ console.log("button clicked");
+
+ formValidation();
+});
+
+let formValidation = () => {
+if (inputElement.value ==="") {
+ alert("post cannot be blank");
+console.log("failure")
+}else{
+ console.log("success");
+ acceptData();
+}
+
+}
 
 
 
-function populatelist(todo){
-let newitem = document.createElement('li');
+let data = [];
 
- // create form and add classlist
- let form = document.createElement('form');
- let task = document.createElement('input');
- task.type = "text"
- task.value = todo;
- task.disabled = "true"
- form.appendChild(task);
- form.classList.add('task');
+let  acceptData = () => {
+     data.push({
+ text:inputElement.value,
+ });
 
-
-
- //create div for the icon and add classlist
- let icons = document.createElement('div');
- icons.classList.add('icons');
-
-//create  and add  check button
-let check = document.createElement('input')
-check.type = 'checkbox'
-icons.appendChild(check);
-
-
-
-//create add edit button
-let editbtn = document.createElement('button');
-editbtn.classList.add('edit');
-let EditElement = document.createElement('i');
-EditElement.classList.add('edit','far','fa-edit');
-editbtn.appendChild(EditElement);
-icons.appendChild(editbtn);
-
-
-//create add delete button
-let deletebtn = document.createElement('button');
-deletebtn.classList.add('delete');
-let deleteElement = document.createElement('i');
-deleteElement.classList.add('delete', 'far', 'fa-times-circle'); 
-deletebtn.appendChild(deleteElement);
-icons.appendChild(deletebtn);
-
-newitem.appendChild(form);
-newitem.appendChild(icons)
+ localStorage.setItem("data",JSON.stringify(data));
  
-//add li to ul
- mylist.appendChild(newitem)
+     console.log(data);
+
+createPost();
+
+};
+
+
+
+let createPost = () => {
+post.innerHTML ="";
+     data.map((x,y) => {
+ return(post.innerHTML += `
+ <div id =${y}>
+ <p>${x.text}</p>
+ <span class = "options">
+      <i onClick ="editTask(this)" class="far fa-edit"></i>
+      <i onClick ="deleteTask(this);createPost ()"class="far fa-times-circle"></i>
+      <i onClick = "checkTask(this)"class="fa fa-circle-thin"></i>
+ </span>
+ </div>
+ `);
+     }) 
+ 
+ inputElement.value = "";
 
 }
+ 
+let deleteTask = (e) => {
+     e.parentElement.parentElement.remove();
+     data.splice(e.parentElement.parentElement.id, 1);
+      localStorage.setItem("data",JSON.stringify(data));
+      console.log(data);
+ 
+};
 
-mylist.addEventListener('click', function (e) {
- e.preventDefault()
- if(e.target.classList.contains('delete')) {
-  e.target.parentElement.parentElement.parentElement.remove()
- }
- if(e.target.classList.contains('edit')){
-  let form = e.target.parentElement.parentElement.previousSibling
-  form.lastChild.disabled = false;
-  form.addEventListener('submit', (event) =>{ 
-e.preventDefault();
- let submit = form.lastChild.disabled("true");
-    
+let editTask = (e) => {
+let selectedTask = e.parentElement.parentElement;
 
-    
+inputElement.value = selectedTask.children[0].innerHTML;
+ deleteTask(e);
 
-}     
+};
 
+let checkTask = (e) =>{
 
-
-  )
+  }
+   
   
- }
-})
 
-
-
-function addtask (todo) { 
- if(todo){
- populatelist(todo);
-}
-
-} 
-
-formElement.addEventListener("submit", (e) =>{ e.preventDefault(); 
- let todo = formElement.add.value.trim();
-
- if(todo.length !=0) {
- addtask(todo);
- formElement.reset();
- }else {
-     alert('please input task');
- }
-})
-
-
+ (() => {
+     data = JSON.parse(localStorage.getItem("data")) || [];
+     console.log(data);
+     createPost();
+    
+})();
 clearitem.addEventListener(
   "click",
   function () {
-    mylist.innerHTML = "";
+    post.innerHTML = "";
     inputElement.value = "";
     clearitem.classList.remove("clear-appear");
   },
   false
 );
-
-
-
-
